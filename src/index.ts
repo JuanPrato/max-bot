c9b6d6571f47a5ae9807901e7976361d7b23677c
JuanPrato <pratojuanmanuel2@gmail.com>
2022-07-31 23:59:33 -0300

    bot configuration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("HOLA");
+import "dotenv/config";
+import { connect } from "mongoose";
+import { client } from "./bot";
+import { loadCaches, loadEvents } from "./utils/helpers";
+
+connect(process.env.BASE_URL!).then(() => {
+  console.log("Connected to db");
+});
+
+loadEvents();
+
+loadCaches();
+
+if (!process.env.TOKEN) {
+  console.log('Bot token not found.');
+  process.exit(1);
+}
+
+client.login(process.env.TOKEN).then(() => {
+  console.log("Listening...");
+});
