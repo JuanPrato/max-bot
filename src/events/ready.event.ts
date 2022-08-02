726b83cfc021021ebb99b429a2e9b4fe0d1df9d8
JuanPrato <pratojuanmanuel2@gmail.com>
2022-08-02 20:25:15 -0300

    set new user profiles on start

--- src/events/ready.event.ts
+++ src/events/ready.event.ts
@@ -1,6 +1,8 @@
 import { Snowflake } from "discord.js";
 import { client } from "../bot";
 import { userModel } from "../models/user.model";
+import {IUser} from "../types/user.type";
+import {IProperties} from "../types/item.type";
 
 const INTERVAL_IN_MS = 1000 * 60 * 10;
 // const FIVE_MINUTES_IN_MS = 10 * 5;
@@ -12,17 +14,17 @@ const getPercentageToRemove = (stat: number, removePercentage: number) => {
   return stat - removePercentage < 0 ? -stat : -removePercentage;
 }
 
-client.on("ready", () => {
+client.on("ready", async () => {
 
     const removePercentage3days = Number(((INTERVAL_IN_MS * 100) / THREE_DAYS_MS).toFixed(2));
     const removePercentage2days = Number(((INTERVAL_IN_MS * 100) / TWO_DAYS_MS).toFixed(2));
     const removePercentage7days = Number(((INTERVAL_IN_MS * 100) / SEVEN_DAYS_MS).toFixed(2));
     
     const notifications = new Map<Snowflake, string[]>();
+    const users = await userModel.find({}).exec();
 
     setInterval( async () => {
         const needNotice = [];
-        const users = await userModel.find({}).exec();
         const usersToUpdate = [];
 
         for (const user of users) {
@@ -52,4 +54,24 @@ client.on("ready", () => {
         await userModel.bulkSave(usersToUpdate);
     }, INTERVAL_IN_MS);
 
+    await client.guilds.fetch();
+    const usersToCreate: IUser[] = [];
+    client.guilds.cache.forEach((guild) => {
+      guild.members.cache.forEach((member) => {
+        if (!users.some(user => user.discordId === member.id)) {
+          usersToCreate.push({
+            discordId: member.id,
+            properties: {
+              food: 0,
+              water: 0,
+              gas: 0,
+              health: 0,
+              service: 0
+            } as IProperties
+          } as IUser);
+        }
+      });
+    });
+
+    await userModel.bulkSave(usersToCreate);
 });
