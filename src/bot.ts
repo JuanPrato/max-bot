5a5759587ef07ff1ba31aaf79fafb46669708fa0
JuanPrato <pratojuanmanuel2@gmail.com>
2022-08-03 09:24:47 -0300

    add guild members intent

--- src/bot.ts
+++ src/bot.ts
@@ -5,6 +5,7 @@ export const client = new Client(
     intents: [
       GatewayIntentBits.Guilds,
       GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.MessageContent
+      GatewayIntentBits.MessageContent,
+      GatewayIntentBits.GuildMembers
     ]
   });
