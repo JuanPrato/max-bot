d799c9033f8d2286d654e71a4a443ae6a4844d05
JuanPrato <pratojuanmanuel2@gmail.com>
2022-08-03 10:10:26 -0300

    add notification; logs on remove resources

--- src/events/ready.event.ts
+++ src/events/ready.event.ts
@@ -1,4 +1,4 @@
-import { Snowflake } from "discord.js";
+import {Guild, GuildMember, Snowflake, User} from "discord.js";
 import { client } from "../bot";
 import { userModel } from "../models/user.model";
 import {IUser} from "../types/user.type";
@@ -10,20 +10,17 @@ const THREE_DAYS_MS = 3 * 24 * 60 * 60 * 1000;
 const TWO_DAYS_MS = 2 * 24 * 60 * 60 * 1000;
 const SEVEN_DAYS_MS = 7 * 24 * 60 * 60 * 1000;
 
-const getPercentageToRemove = (stat: number, removePercentage: number) => {
-  return stat - removePercentage < 0 ? -stat : -removePercentage;
-}
+const removePercentage3days = Number(((INTERVAL_IN_MS * 100) / THREE_DAYS_MS).toFixed(2));
+const removePercentage2days = Number(((INTERVAL_IN_MS * 100) / TWO_DAYS_MS).toFixed(2));
+const removePercentage7days = Number(((INTERVAL_IN_MS * 100) / SEVEN_DAYS_MS).toFixed(2));
+
 
 client.on("ready", async () => {
 
-    const removePercentage3days = Number(((INTERVAL_IN_MS * 100) / THREE_DAYS_MS).toFixed(2));
-    const removePercentage2days = Number(((INTERVAL_IN_MS * 100) / TWO_DAYS_MS).toFixed(2));
-    const removePercentage7days = Number(((INTERVAL_IN_MS * 100) / SEVEN_DAYS_MS).toFixed(2));
-    
     const notifications = new Map<Snowflake, string[]>();
     let users = await userModel.find({}).exec();
     setInterval( async () => {
-        const needNotice = [];
+        const needNotice: User[] = [];
         const usersToUpdate = [];
 
         users = await userModel.find({}).exec();
@@ -38,7 +35,15 @@ client.on("ready", async () => {
           const healthToRemove = getPercentageToRemove(health, removePercentage7days);
           const serviceToRemove = getPercentageToRemove(service, removePercentage7days);
 
+          if (food - foodToRemove < 5 || water - waterToRemove < 5 || gas - gasToRemove < 5 || health - healthToRemove < 5 || service - serviceToRemove < 5) {
+            const discordUser = client.users.cache.get(user.discordId)!;
+
+            needNotice.push(discordUser);
+          }
+
+          console.log(`${user.discordId} - ${food} - ${water} - ${gas} - ${health} - ${service}`);
           if (foodToRemove < 0 || waterToRemove < 0 || gasToRemove < 0 || healthToRemove < 0 || serviceToRemove < 0) {
+            console.log("USER => ", user);
             user.updateOne({
                 $inc: {
                     "properties.food": foodToRemove,
@@ -53,6 +58,9 @@ client.on("ready", async () => {
         }
 
         await userModel.bulkSave(usersToUpdate);
+
+        await notifyOnLowResource(needNotice);
+
     }, INTERVAL_IN_MS);
 
     await client.guilds.fetch();
@@ -81,3 +89,21 @@ client.on("ready", async () => {
       await userModel.bulkSave(usersToCreate);
     }
 });
+
+const getPercentageToRemove = (stat: number, removePercentage: number) => {
+  return stat - removePercentage < 0 ? -stat : -removePercentage;
+}
+
+const notifyOnLowResource = async (usersToNotify: User[]) => {
+
+    for (const user of usersToNotify ) {
+
+      const dm = user.dmChannel;
+
+      if (!dm) continue;
+
+      dm.send(`Cuidado, tus recursos están bajos. Utiliza el comando -est para ver tu estado.`);
+
+    }
+
+}
