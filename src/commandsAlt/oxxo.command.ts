51c39e2f5cff13479d8016c73ba8b060a098c973
JuanPrato <pratojuanmanuel2@gmail.com>
2022-08-08 15:10:37 -0300

    set correct oxxo text

--- src/commandsAlt/oxxo.command.ts
+++ src/commandsAlt/oxxo.command.ts
@@ -8,23 +8,20 @@ export default class OxxoCommand extends BaseCommand {
   static command = "inioxxo";
 
   static async run(message: Message) {
-    const textBase = "Lleva el camión a (u) y cargale (c) , (c) , (c) y descargarlo en (u)";
+    const textBase = "lleva la camioneta a (u) y cargale (c) y descargalo en el oxxo";
 
     const places = [ 'eclipse towers', 'barrios verdes', 'comisaría alcantarillas', 'aéreopuerto', 'barrios morados', 'barrios amarillos', 'maze bank', 'casino', 'mecánico central', 'rueda de la fortuna de la playa', 'muelles', 'campo de golf' ];
     const things = [ 'cajas de alcohol', 'pales de comida', 'hielo', 'cajas de dulces', 'gasolina', 'papel de baño', 'telefonos', 'cajas de bebidas', 'cajas con periódicos', 'cajas de cigarros', 'condones'];
 
-    const selectedPlaces = getRandomElementsWithOutRepetition(places, 2);
-    const selectedThings = getRandomElementsWithOutRepetition(things, 3);
+    const selectedPlaces = getRandomElementsWithOutRepetition(places, 1);
+    const selectedThings = getRandomElementsWithOutRepetition(things, 1);
 
     await message.reply({
       embeds: [
         createEmbedAlert(
           textBase
             .replace("(u)", selectedPlaces[0])
-            .replace("(u)", selectedPlaces[1])
             .replace("(c)", selectedThings[0])
-            .replace("(c)", selectedThings[1])
-            .replace("(c)", selectedThings[2])
         )
       ]
     });
