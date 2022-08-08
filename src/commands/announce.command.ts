a3ba0dfa7fc9f2e4be8d99e6badc36bebdd2e9c9
JuanPrato <pratojuanmanuel2@gmail.com>
2022-08-08 16:44:05 -0300

    announce command

---
+++ src/commands/announce.command.ts
@@ -0,0 +1,30 @@
+import BaseCommand from "./base.command";
+import {Message, TextChannel} from "discord.js";
+import {CommandType} from "../types/command.type";
+import {createEmbedAlert} from "../utils/embed.utils";
+
+export default class AnnounceCommand extends BaseCommand {
+
+  static command = "announce";
+  static adminOnly = true;
+  static async run (message: Message, command: CommandType) {
+
+    const channel = message.mentions.channels.first();
+
+    if (!channel) {
+      throw new Error("Debes mencionar un canal");
+    }
+
+    command.args.shift();
+
+    const announce = command.args.join(" ");
+
+    if (channel.isTextBased()) {
+      await (channel as TextChannel).send({
+        embeds: [createEmbedAlert(announce, undefined, true)],
+      });
+    }
+
+  }
+
+}
--- src/utils/embed.utils.ts
+++ src/utils/embed.utils.ts
@@ -1,6 +1,7 @@
 import {Colors, EmbedBuilder} from "discord.js";
 
-export const createEmbedAlert = (title: string, color?: number) => EmbedBuilder.from({
+export const createEmbedAlert = (title: string, color?: number, timestamp?: boolean) => EmbedBuilder.from({
   title,
-  color: color || Colors.Green
+  color: color || Colors.Green,
+  timestamp: timestamp ? new Date().toISOString() : undefined,
 });
